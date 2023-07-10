4de93c7b60ab7ab0ec82b6f85d486a3b4dece961
fouad258 <fouad16adam@gmail.com>
2023-07-10 14:52:48 +0100

    making the last changes on my javascript code

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+let accessValue = '0';
 const access = document.getElementById('result');
-      
+
 // Function to add a digit to the display
 function addToDisplay(digit) {
   if (access.value === '') {
-    access.value = digit; 
+    access.value = digit;
   } else if (digit === '.' && access.value.includes('.')) {
     access.value += digit;
   } else {
@@ -11,21 +12,42 @@ function addToDisplay(digit) {
   }
 }
 
-// Function to add an operator to the display
+function accessDisplay() {
+  access.innerText = accessValue;
+  if (accessValue.length > 9) {
+    access.innerText = accessValue.substring(0, 9);
+  }
+}
+
+accessDisplay();
+
+function inputDecimal(dot) {
+  if (accessValue === '0') {
+    accessValue = '0' + dot;
+  } else if (!accessValue.includes(dot)) {
+    accessValue += dot;
+  }
+}
+
 function addOperator(operator) {
   access.value += operator;
-} 
+}
+
 function isOperator(char) {
   return char === '+' || char === '-' || char === '*' || char === '/';
-} 
+}
 
 function factorial(a) {
-	if (a == 0) return 1;
-	let product = 1;
-	for (let i = a; i > 0; i--) {
-	  product *= i;
-	}
-	return product;
+  if (a === 0) return 1;
+  let product = 1;
+  for (let i = a; i > 0; i--) {
+    product *= i;
+  }
+  return product;
+}
+
+function isDigit(char) {
+  return /[0-9]/.test(char);
 }
 
 // Function to evaluate the expression and display the result
@@ -37,64 +59,71 @@ function calculate() {
     access.value = 'Invalid expression';
     return;
   }
+
   let result = null;
   let operator = null;
-  let currentNumber = null;
-  
-  function isDigit(char) {
-    return /[0-9]/.test(char);
-  }
+  let currentNumber = '';
 
   for (let i = 0; i < expression.length; i++) {
     const char = expression[i];
+
     if (isOperator(char)) {
-      if (result === null) {
-        result = parseFloat(expression.substring(0, i));
+      if (currentNumber !== '') {
+        const number = parseFloat(currentNumber);
+        if (result === null) {
+          result = number;
+        } else {
+          result = performOperation(result, operator, number);
+        }
+        currentNumber = '';
       }
       operator = char;
-      currentNumber = null;
-    } else if (currentNumber === null && isDigit(char)) {
-      currentNumber = char;
-    } else if (currentNumber !== null && isDigit(char)) {
-      currentNumber += char;
     } else {
-      // Invalid character
-      access.value = 'Invalid expression';
-      return;
+      currentNumber += char;
     }
   }
-  // Evaluate the last operation
-  if (operator && currentNumber) {
-    const secondNumber = parseFloat(currentNumber);
 
-    switch (operator) {
-      case '+':
-        result += secondNumber;
-        break;
-      case '-':
-        result -= secondNumber;
-        break;
-      case '*':
-        result *= secondNumber;
-        break;
-      case '/':
-        if (secondNumber === 0) {
-          access.value = 'Division by zero';
-          return;
-        }
-        result /= secondNumber;
-        break;
-      default:
-        access.value = 'Invalid operator';
-        return;
+  if (currentNumber !== '') {
+    const number = parseFloat(currentNumber);
+    if (result === null) {
+      result = number;
+    } else {
+      result = performOperation(result, operator, number);
     }
   }
 
   access.value = result !== null ? result.toString() : 'Error';
+  access.value = roundedResult !== null ? roundedResult.toString() : 'Error';
 }
+
+
+
+
+
+
+function performOperation(a, operator, b) {
+  switch (operator) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      if (b === 0) {
+        access.value = 'Division by zero';
+        throw new Error('Division by zero');
+      }
+      return a / b;
+    default:
+      throw new Error('Invalid operator');
+  }
+}
+
 function roundToTwoDecimals(number) {
   return Math.round((number + Number.EPSILON) * 100) / 100;
 }
+
 // Function to validate the expression
 function validateExpression(expression) {
   const pattern = /^(-?\d*\.?\d+)\s*([-+*/])\s*(-?\d*\.?\d+)$/;
@@ -107,8 +136,8 @@ function clearDisplay() {
 }
 
 //Add Event handler for keyboard keys
-window.addEventListener('keydown', function(e) {
-  const key = e.key; 
+window.addEventListener('keydown', function (e) {
+  const key = e.key;
 
   // Check the key plus its value on the keyboard
   switch (key) {
